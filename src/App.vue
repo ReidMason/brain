241fc8b7823b6d462ee4fc96e699c9e538423556
Jacques <jacques.marseilles@gmail.com>
2020-06-19 18:16:34 +0100

    Brain first commit from Jac :)

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <h1>Brain v1</h1>
+    <div v-html="text" ></div>
+    <searchBar @listUpdated="(newItem) => {items.push(newItem)}" />
+    <item v-for="item in items" :key="item" :text="item" />
+  </div>
+</template>
+
+<script>
+import searchBar from './components/searchBar.vue'
+import item from './components/item.vue'
+
+export default {
+  name: 'App',
+  components: {
+    searchBar,
+    item
+  },
+  data: function () {
+    return {
+      items: [],
+      text: ''
+    }
+  }
+}
+</script>
---
+++ src/components/searchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <form @submit.prevent="$emit('listUpdated', fieldInput)" >
+    <label for="prime">Search</label>
+    <input type="text" name="prime" v-model="fieldInput">
+    <button type="submit" >Submit</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        fieldInput: ''
+      }
+    }
+  }
+</script>
